c9f2fd716d9431841b862386dbedcdc2e28926ef
Manuela <mnzali13@gmail.com>
2024-09-13 18:47:00 -0500

    final update

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -7,18 +7,19 @@ from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 
 from flask import Flask, jsonify
+from datetime import datetime as dt
 
 
 #################################################
 # Database Setup
 #################################################
-engine = create_engine("sqlite:///C:\Users\bryan\OneDrive - Dallas College\Documents\Manuela\Homework\Module10Challenge\sqlalchemy-challenge\SurfsUp\Resources\hawaii.sqlite")
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 #engine = create_engine("sqlite:///.../Resources/hawaii.sqlite")
 
 # reflect an existing database into a new model
 Base = automap_base()
 
-# reflect the tables
+# reflect the tablesdid I
 Base.prepare(autoload_with=engine)
 
 #Base.classes.keys()
@@ -65,7 +66,7 @@ def precipitation():
 
 @app.route("/api/v1.0/stations")
 def stations():
-    stations_collection = session.query(Stations.station).all()
+    stations_collection = session.query(Station.station).all()
     list_of_stations = [station[0] for station in stations_collection]
 
     return jsonify(list_of_stations)
@@ -73,7 +74,7 @@ def stations():
 @app.route("/api/v1.0/tobs")
 def tobs():
     #Query the dates and temperature observations of the most-active station for the previous year of data.
-    begin_date = dt.datetime.strptime(most_recent_date, '%Y-%m-%d').date() - dt.timedelta(days=365)
+    begin_date = dt.strptime(most_recent_date, '%Y-%m-%d').date() - dt.timedelta(days=365)
     twelve_months_data = session.query(Measurement.tobs).filter(Measurement.date >= begin_date, Measurement.date <= most_recent_date, Measurement.station == "USC00519281").all()
     active_data = [{'Date': date, 'Temperature': temp} for date, temp in twelve_months_data]
 
@@ -81,34 +82,36 @@ def tobs():
     return jsonify(active_data)
 
 @app.route("/api/v1.0/<start>")
-def start():
+def start(start):
     #Return a JSON list of the minimum temperature, the average temperature, and the maximum temperature for a specified start range.
     try:
-        start_date = dt.datetime.strptime(start, '%Y-%m-%d')
+        start_date = dt.strptime(start, '%Y-%m-%d')
     except ValueError:
-        return jsonify({"error": "Invalid start date format. Use YYYY-MM-DD."}), 400
+        return jsonify({"error": "wrong date format. Use YYYY-MM-DD."}), 400
     #For a specified start, calculate TMIN, TAVG, and TMAX for all the dates greater than or equal to the start date.
     calculate = session.query(
         func.min(Measurement.tobs).label('TMIN'),
         func.avg(Measurement.tobs).label('TAVG'),
         func.max(Measurement.tobs).label('TMAX')).filter(Measurement.date >= start_date).all()
     #For a specified start date and end date, calculate TMIN, TAVG, and TMAX for the dates from the start date to the end date, inclusive.
-    calculation_results = results[0] if results else {"TMIN": None, "TAVG": None, "TMAX": None}
-    
-    return jsonify(calculation_results)
+    calculation_results = calculate[0] if calculate else {"TMIN": None, "TAVG": None, "TMAX": None}
+    print(calculate)
+    print(type(calculation_results))
+    results = list(np.ravel(calculate))
+    return jsonify(results)
 
 @app.route("/api/v1.0/<start>/<end>")
-def start_end():
+def start_end(start,end):
 
     #Return a JSON list of the minimum temperature, the average temperature, and the maximum temperature for a specified start-end range.
     try:
-        start_date = dt.datetime.strptime(start, '%Y-%m-%d')
-        end_date = dt.datetime.strptime(end, '%Y-%m-%d')
+        start_date = dt.strptime(start, '%Y-%m-%d')
+        end_date = dt.strptime(end, '%Y-%m-%d')
     except ValueError:
-        return jsonify({"error": "Invalid date format. Use YYYY-MM-DD."}), 400
+        return jsonify({"error": "wrong date format. Use YYYY-MM-DD."}), 400
 
     if start_date > end_date:
-        return jsonify({"error": "Start date must be before or equal to end date."}), 400
+        return jsonify({"error": "Start date must fall within range."}), 400
 
     #For a specified start, calculate TMIN, TAVG, and TMAX for all the dates greater than or equal to the start date.
     start_end = session.query(
@@ -118,9 +121,11 @@ def start_end():
     ).filter(Measurement.date >= start_date, Measurement.date <= end_date).all()
 
     #For a specified start date and end date, calculate TMIN, TAVG, and TMAX for the dates from the start date to the end date, inclusive.
-    start_end_results = results[0] if results else {"TMIN": None, "TAVG": None, "TMAX": None}
-    
-    return jsonify(start_end_results)
+    # start_end_results = results[0] if results else {"TMIN": None, "TAVG": None, "TMAX": None}
+    #print(start_end)
+    #print(type(start_end_results))
+    results = list(np.ravel(start_end))
+    return jsonify(results)
 
 
 if __name__ == '__main__':
